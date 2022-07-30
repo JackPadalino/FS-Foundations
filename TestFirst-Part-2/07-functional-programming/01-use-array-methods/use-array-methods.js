bbcc0aec6f7611474fcf74c5ad5b99170291481b
JackPadalino <pada0867@gmail.com>
2022-07-30 13:29:45 -0400

    added solutions for first two challenges

--- TestFirst-Part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
+++ TestFirst-Part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
@@ -1,1 +1,36 @@
 /* eslint-disable no-unused-vars */
+
+//~~~~~indexAndValue~~~~~//
+const indexAndValue=(arr)=>{
+    /*
+    //Using an imperative approach
+    const newArr=[];
+    for(let i=0;i<arr.length;i++){
+        const obj={
+            index:i,
+            value:arr[i]
+        };
+        newArr.push(obj);
+    };
+    return newArr;
+    */
+    const newArr=arr.map(element=>({index:arr.indexOf(element),value:element}));
+    return newArr;
+};
+
+//~~~~~swapCase~~~~~//
+const capitalize=aString=>aString.toUpperCase();
+const swapCase=aString=>{
+    const stringArray=aString.split(' ');
+    const capsStringArray=stringArray.map((str,index)=>index%2===0 ? capitalize(str) : str);
+    /*
+        if(index%2===0){
+            return capitalize(str);
+        }else{
+            return str;
+        };
+    */
+    const finalString=capsStringArray.join(' ');
+    return finalString;
+};
+
