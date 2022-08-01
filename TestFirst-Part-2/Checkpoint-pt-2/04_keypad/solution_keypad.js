48830171c4ffaba80434b036933dc82f8bf5fb0f
JackPadalino <pada0867@gmail.com>
2022-08-01 19:15:14 -0400

    updated solution for solution_keypad.js

--- TestFirst-Part-2/Checkpoint-pt-2/04_keypad/solution_keypad.js
+++ TestFirst-Part-2/Checkpoint-pt-2/04_keypad/solution_keypad.js
@@ -1,25 +1,29 @@
 /* eslint-disable no-unused-vars */
 
-//~~~~~presses~~~~~//
-const presses=str=>{
-    const keypadValues={
+const keypadValues={
         1:['a','d','g','j','m','p','t','w',' ','1'],
         2:['b','e','h','k','n','q','u','x','0'],
         3:['c','f','i','l','o','r','v','y'],
         4:['s','z','2','3','4','5','6','8'],
         5:['7','9']
     };
-    // getCharValue retrieves a character's key if it is in a pair's array
-    const getCharValue=char=>{
-        for(const [key,val] of Object.entries(keypadValues)){
-                if(val.includes(char)){
-                    return Number(key);
-                };
+
+// getCharValue retrieves a character's key if it is in a pair's array
+const getCharValue=char=>{
+    for(const [key,val] of Object.entries(keypadValues)){
+            if(val.includes(char)){
+                return Number(key);
             };
-    };
-    const sumItUp=(total,currentNum)=>{
-        return total+currentNum;
         };
+};
+
+// callback function to be used in 'reduce' function below
+const sumItUp=(total,currentNum)=>{
+    return total+currentNum;
+    };
+
+//~~~~~presses~~~~~//
+const presses=str=>{
     /*
     const charsArr=str.split('').map(char=>char.toLowerCase());
     
