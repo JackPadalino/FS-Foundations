2b533e2e59736b7ba34293c661437dec2f47a0c6
JackPadalino <pada0867@gmail.com>
2022-08-04 17:11:46 -0400

    added solution for pig-latin.js

--- TestFirst-Review-V2/basics/01-pig-latin/pig-latin.js
+++ TestFirst-Review-V2/basics/01-pig-latin/pig-latin.js
@@ -1,1 +1,27 @@
 /* eslint-disable no-unused-vars */
+
+const vowelsArr=['a','e','o','i','u'];
+const prefiexArr=['sch','qu','squ'];
+
+//~~~~~pigify~~~~~//
+// ThIs CoDe FaIlS tHe LaSt ThReE tEsTs //
+const pigify=str=>{
+    const strArr=str.split(' ');
+    const pigifiedWordsArr=[];
+    for(let word of strArr){
+        let counter=0;
+        for(let letter of word){
+            if(vowelsArr.includes(letter)){
+                break;
+            }else{
+                counter++;
+            };
+        };
+        const slice1=word.slice(0,counter);
+        const slice2=word.slice(counter,word.length);
+        const pigifiedWord=`${slice2}${slice1}ay`
+        pigifiedWordsArr.push(pigifiedWord);
+    };
+    const finalStr=pigifiedWordsArr.join(' ');
+    return finalStr;
+};
