9d84e88e3cca29662a047ff8cc0b1f388dd96367
JackPadalino <pada0867@gmail.com>
2022-08-04 18:58:18 -0400

    updated solution for vowel-count.js

--- TestFirst-Review-V2/basics/00-vowel-count/vowel-count.js
+++ TestFirst-Review-V2/basics/00-vowel-count/vowel-count.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 
 //~~~~~vowelsCount~~~~~//
+/*
 const vowelsCount=(str)=>{
     const vowelsArr=['a','e','i','o','u'];
     const vowelsObj={
@@ -23,4 +24,21 @@ const vowelsCount=(str)=>{
     const total=Object.values(vowelsObj).reduce(sumItUp,0);
     vowelsObj.total=total;
     return vowelsObj;
+};
+*/
+
+// solution using reduce
+const vowelsCount=(str)=>{
+    const vowels='aeiou';
+    str = str.toLowerCase();
+    // callback function to count the frequency of vowels in the string and increment the
+    // object
+    const countEm=(obj, current)=>{
+        if(vowels.includes(current)){
+            obj[current]++;
+            obj['total']++;
+        };
+        return obj;
+    };
+    return str.split('').reduce(countEm,{a:0,e:0,i:0,o:0,u:0,total:0});
 };
