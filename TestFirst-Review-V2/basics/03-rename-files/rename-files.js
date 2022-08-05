e92e07302b3169bf7c41ffd0a9f122063908d4d1
JackPadalino <pada0867@gmail.com>
2022-08-05 16:41:15 -0400

    added solution for rename-files.js

--- TestFirst-Review-V2/basics/03-rename-files/rename-files.js
+++ TestFirst-Review-V2/basics/03-rename-files/rename-files.js
@@ -1,1 +1,38 @@
 /* eslint-disable no-unused-vars */
+
+const renameFiles=fileArr=>{
+    const counterObj={};
+    const newFileArr=[];
+    let seenFirstInstance=false;
+    let fileEnding=1;
+    for(let file of fileArr){
+        if(file in counterObj){
+            counterObj[file]++;
+        }else{
+            counterObj[file]=1;
+        };
+    };
+    for(let file of fileArr){
+        if(counterObj[file]>1){
+            if(seenFirstInstance){
+                while(true){
+                    const testFile=`${file}(${fileEnding})`;
+                    if(fileArr.includes(testFile)){
+                        fileEnding++;
+                    }else{
+                        break;
+                    };
+                };
+                const newFile=`${file}(${fileEnding})`;
+                newFileArr.push(newFile)
+                fileEnding++;
+            }else{
+                newFileArr.push(file);
+                seenFirstInstance=true;
+            };
+        }else{
+            newFileArr.push(file);
+        };
+    };
+    return newFileArr;
+};
