748787631a23bccf0afaf338907dd1387a8ebd56
JackPadalino <pada0867@gmail.com>
2022-08-06 13:41:08 -0400

    added solution for array-prototype.js

--- TestFirst-Review-V2/classes/01-array-prototype/array-prototype.js
+++ TestFirst-Review-V2/classes/01-array-prototype/array-prototype.js
@@ -1,1 +1,28 @@
 /* eslint-disable no-unused-vars */
+
+//~~~~~Array.prototype.maap~~~~~//
+Array.prototype.maap=function(func){
+    const newArr=[];
+    this.forEach(item=>newArr.push(func(item)));
+    return newArr;
+};
+
+//~~~~~Array.prototype.fiilter~~~~~//
+Array.prototype.fiilter=function(func){
+    const newArr=[];
+    this.forEach(item=>{
+        if(func(item)){
+            newArr.push(item);
+        };
+    });
+    return newArr;
+};
+
+//~~~~~Array.prototype.reeduce~~~~~//
+Array.prototype.reeduce=function(func,startValue){
+    let value=startValue;
+    this.forEach(item=>{
+        value=func(value,item)
+    });
+    return value;
+};
