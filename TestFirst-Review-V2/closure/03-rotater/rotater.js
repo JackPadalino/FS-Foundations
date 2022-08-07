71a16bd077e9578d3a1d80f368bd8f111a0e3e04
JackPadalino <pada0867@gmail.com>
2022-08-07 16:34:52 -0400

    added solution for rotater.js

--- TestFirst-Review-V2/closure/03-rotater/rotater.js
+++ TestFirst-Review-V2/closure/03-rotater/rotater.js
@@ -1,1 +1,44 @@
 /* eslint-disable no-unused-vars */
+
+//~~~~~solution 1 using splice and string literals~~~~~//
+const rotater=str=>{
+    let forward=false;
+    return function(num){
+        if(num===str.length){
+            forward=!forward;
+        };
+        if(!forward){
+            return `${str.slice(num,str.length)}${str.slice(0,num)}`
+        };
+        if(forward){
+            return `${str.slice(str.length-num,str.length)}${str.slice(0,str.length-num)}`
+        };  
+    };
+};
+
+/*
+//~~~~~solution 2 using loops and array methods~~~~~//
+const rotater=str=>{
+    let forward=false;
+    return function(num){
+        if(num===str.length){
+            forward=!forward;
+        };
+        const strArr=str.split('');
+        if(!forward){
+            for(let i=0;i<num;i++){
+                const item=strArr.shift();
+                strArr.push(item);
+            };
+        };
+        if(forward){
+            for(let i=0;i<num;i++){
+                const item=strArr.pop();
+                strArr.unshift(item);
+            };
+        };
+        const finalStr=strArr.join('');
+        return finalStr; 
+    };
+};
+*/
