367fb151490539493af6f8f6093c14a402d7204c
JackPadalino <pada0867@gmail.com>
2022-08-07 14:41:48 -0400

    added solution for make-iterator.js

--- TestFirst-Review-V2/closure/02-make-iterator/make-iterator.js
+++ TestFirst-Review-V2/closure/02-make-iterator/make-iterator.js
@@ -1,1 +1,18 @@
 /* eslint-disable no-unused-vars */
+
+const makeIterator=arr=>{
+    let index=0;
+    const obj={
+        getNext(){
+            //index++;
+            return {
+                value:arr[index++],
+                done:index>arr.length
+            };
+        },
+        getIndex(){
+            return index;
+        }
+    };
+    return obj;
+};
