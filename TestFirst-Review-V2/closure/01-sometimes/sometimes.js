9d22f5a65fab79f5ea8d07e6950f1da25967b525
JackPadalino <pada0867@gmail.com>
2022-08-06 22:11:07 -0400

    added solution for sometimes.js

--- TestFirst-Review-V2/closure/01-sometimes/sometimes.js
+++ TestFirst-Review-V2/closure/01-sometimes/sometimes.js
@@ -1,1 +1,19 @@
 /* eslint-disable no-unused-vars */
+
+const sometimes=func=>{
+    let called = 1;
+    return function() {
+        if (called <= 3) {
+            called++;
+            return func(...arguments);
+        }else{
+            if(called%2===0){
+                called++;
+                return 'I do not know!';
+            }else{
+                called++;
+                return func(...arguments);
+            };
+        };  
+    };
+};
