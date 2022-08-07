a818faa497ef94fdb3b4b777e1274f31345a081d
JackPadalino <pada0867@gmail.com>
2022-08-06 20:43:53 -0400

    added solution for event-emitter.js

--- TestFirst-Review-V2/classes/03-event-emitter/event-emitter.js
+++ TestFirst-Review-V2/classes/03-event-emitter/event-emitter.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+
+class MyEventEmitter{
+    constructor(){
+        this.events={};
+    };
+    addListener(event,func){
+        if(event in this.events){
+            this.events[event].push(func);
+        }else{
+            this.events[event]=[func];
+        };
+    };
+    emit(event,...extraArgs){
+        this.events[event].forEach(func=>func(...extraArgs));
+    };
+};
