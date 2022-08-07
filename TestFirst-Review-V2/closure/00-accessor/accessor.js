868443c18e7cd1d55dc171ac2a606829e4927057
JackPadalino <pada0867@gmail.com>
2022-08-06 21:11:14 -0400

    added solution for accessor.js

--- TestFirst-Review-V2/closure/00-accessor/accessor.js
+++ TestFirst-Review-V2/closure/00-accessor/accessor.js
@@ -1,1 +1,7 @@
 /* eslint-disable no-unused-vars */
+
+const accessor=obj=>{
+    //const newFunc=(key,newValue)=>newValue?obj[key]=newValue:obj[key];
+    //return newFunc;
+    return (key,newValue)=>newValue ? obj[key]=newValue : obj[key];
+};
