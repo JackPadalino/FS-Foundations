bb8c8a75973689a4435ec189926cf6e63fcc877d
JackPadalino <pada0867@gmail.com>
2022-08-07 20:44:41 -0400

    added solution for map-quest.js

--- TestFirst-Review-V2/basics/04-map-quest/solution-map-quest.js
+++ TestFirst-Review-V2/basics/04-map-quest/solution-map-quest.js
@@ -1,32 +1,15 @@
 /* eslint-disable no-unused-vars */
 
-const mapQuest=arr=>{
-    const dirObj={
-        'N':1,
-        'S':-1,
-        'E':2,
-        'W':-2
+function mapQuest(route) {
+    let opposite = {
+      N: 'S',
+      E: 'W',
+      S: 'N',
+      W: 'E',
     };
-    const finalArr=[];
-    const arrLength=arr.length;
-    const [first,last]=[arr[0],arr[arr.length-1]];
-    const newArr=arr;
-    newArr.splice(0,0,first)
-    newArr.splice(-1,0,last,last,last);
-    for(let a=1;a<arrLength+1;a++){
-        let total=dirObj[newArr[a]]+dirObj[newArr[a-1]];
-        if(total===0){
-            //pass
-        }else{
-            let total1=dirObj[newArr[a]]+dirObj[newArr[a+1]];
-            let total2=dirObj[newArr[a]]+dirObj[newArr[a+2]];
-            let total3=dirObj[newArr[a]]+dirObj[newArr[a+3]];
-            if(total1===0||total2===0||total3===0){
-                //pass
-            }else{
-                finalArr.push(arr[a]);
-            };
-        };
-    };
-    return finalArr;
-};
+    return route.reduce(function (directions, dir) {
+      if (directions[directions.length - 1] === opposite[dir]) directions.pop();
+      else directions.push(dir);
+      return directions;
+    }, []);
+  }
