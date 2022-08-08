8f889d746f9dd45a8a875a27ff213a352da12c3a
JackPadalino <pada0867@gmail.com>
2022-08-07 20:40:28 -0400

    added solution for do-not-include.js

--- TestFirst-Review-V2/this-keyword-context/01-do-not-include/do-not-include.js
+++ TestFirst-Review-V2/this-keyword-context/01-do-not-include/do-not-include.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars, no-extend-native */
+
+Array.prototype.doNotInclude=function(args){
+    if(!Array.isArray(args)) args=[args];
+    return this.filter((value,index)=>{
+        if(!args.includes(index)){
+            return value;
+        };
+    });
+};
