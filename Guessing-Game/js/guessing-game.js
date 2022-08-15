d624aa8a288e63e7530f7d4ccf9f065a205d2baa
JackPadalino <pada0867@gmail.com>
2022-08-15 16:02:13 -0400

    first commit

---
+++ Guessing-Game/js/guessing-game.js
@@ -0,0 +1,9 @@
+/* 
+
+Write your guess-game code here! Don't forget to look at the test specs as a guide. You can run the specs
+by running "testem".
+
+In this file, you will also include the event listeners that are needed to interact with your HTML file when
+a user clicks a button or adds a guess to the input field.
+
+*/
