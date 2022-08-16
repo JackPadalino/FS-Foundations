88364e295dc27acdbed3d95fcd640e53be5238b6
JackPadalino <pada0867@gmail.com>
2022-08-16 13:18:36 -0400

    updated CSS

--- Guessing-Game/js/guessing-game.js
+++ Guessing-Game/js/guessing-game.js
@@ -7,3 +7,16 @@ In this file, you will also include the event listeners that are needed to inter
 a user clicks a button or adds a guess to the input field.
 
 */
+
+function getGuessAndUpdate(element){
+    const text=element.value;
+    element.value='';
+    alert(typeof text);
+};
+
+let submitGuessButton=document.getElementById('submit-guess-button');
+
+submitGuessButton.addEventListener('click',function(){
+    const element=document.getElementById('enter-guess-form');
+    getGuessAndUpdate(element);
+});
