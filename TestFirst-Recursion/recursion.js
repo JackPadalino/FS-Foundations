050e080d66e966573d156a344f98a272464b4ad2
JackPadalino <pada0867@gmail.com>
2022-08-21 20:41:32 -0400

    added solutions to recursion.js

--- TestFirst-Recursion/recursion.js
+++ TestFirst-Recursion/recursion.js
@@ -4,34 +4,51 @@
 
 /* eslint-disable no-unused-vars */
 
-function factorial() {
+factorialIterative=n=>{
+    let result=1;
+    for(let i=n;i>0;i--){
+        result=result*i;
+    };
+    return result;
+};
 
-}
+function factorial(n){
+    if(n<=1) return 1;
+    else return n*factorial(n-1);
+};
 
-function sumTheDigits() {
+function sumTheDigits(arr){
+    if(arr.length===0) return 0;
+    else return arr[0]+sumTheDigits(arr.slice(1));
+};
 
-}
+function countTheVowels(str){
+    const vowels='aeiouAEIOU';
+    let totalVowels=0;
+    if(str.length===0) return 0;
+    if(vowels.includes(str[0])) totalVowels++;
+    return totalVowels+countTheVowels(str.slice(1));
+};
 
-function countTheVowels() {
+function recSmallestInt(arr){
+    // array needs to shrink after each call
+    if(arr.length===1)return arr[0];
+    if(arr[0]>arr[arr.length-1]) return recSmallestInt(arr.slice(1));
+    else return recSmallestInt(arr.slice(0,arr.length-1));
+};
 
-}
+function fib(){
 
-function recSmallestInt() {
+};
 
-}
+function stringify(){
 
-function fib() {
+};
 
-}
+function search(){
 
-function stringify() {
+};
 
-}
+function recursiveMap(){
 
-function search() {
-
-}
-
-function recursiveMap() {
-
-}
+};
